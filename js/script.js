9162fe2542626d2475151f3cea2c4be5cbfc2334
Glitch (codenation-rps-ko) <none>
2021-01-19 23:05:59 +0000

    🚅🎖 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -5,5 +5,23 @@
 //GLOBAL VARIABLES
 /* global $ */
 
+let user_choice = '';
+let computer_choice = '';
+let winner = '';
+  
+let randomNumber = 0;
 
-
+$("#shoot").click(function(){
+  let randomNumber = Math.random();
+  user_choice = $("#input").val();
+  $("#userChoice").html(user_choice);
+  if ( randomNumber <= .33 ){
+    computer_choice = "rock";
+  } else if (
+  (randomNumber >.33 && randomNumber <.66)){
+    computer_choice = "paper";
+  } else {
+    computer_choice ="scissors"
+  }
+  
+});
