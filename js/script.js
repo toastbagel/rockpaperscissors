a9428f541347d95d18450f891b89f14d455fde65
Glitch (codenation-rps-ko) <none>
2021-01-26 22:14:41 +0000

    🍛🍫 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-
 let user_choice = '';
 let computer_choice = '';
 let winner = '';
@@ -12,9 +11,13 @@ let winner = '';
 let randomNumber = 0;
 
 $("#shoot").click(function(){
+  let valid = false;
   let randomNumber = Math.random();
+  console.log(randomNumber)
   user_choice = $("#input").val();
   $("#userChoice").html(user_choice);
+  
+  
   if ( randomNumber <= .33 ){
     computer_choice = "rock";
   } else if (
@@ -23,5 +26,51 @@ $("#shoot").click(function(){
   } else {
     computer_choice ="scissors"
   }
+  $("#computerChoice").html(computer_choice)
   
+  if (user_choice == "rock") {
+    valid = true;
+    if (computer_choice == "rock"){
+      $("#result").html("Tie");
+    } 
+    if (computer_choice == "paper"){
+      $("#result").html("Computer Wins");
+    } 
+     if (computer_choice == "scissors"){
+      $("#result").html("User Wins");
+    } 
+  }
+  
+  
+ if (user_choice == "paper") {
+   valid = true;
+    if (computer_choice == "rock"){
+      $("#result").html("User Wins");
+    } 
+    if (computer_choice == "paper"){
+      $("#result").html("Tie");
+    } 
+     if (computer_choice == "scissors"){
+      $("#result").html("Computer Wins");
+    } 
+  }
+  
+  
+  if (user_choice == "scissors") {
+    valid = true;
+    if (computer_choice == "rock"){
+      $("#result").html("Computer Wins");
+    } 
+    if (computer_choice == "paper"){
+      $("#result").html("User Wins");
+    } 
+     if (computer_choice == "scissors"){
+      $("#result").html("Tie");
+    } 
+  }
+  
+  if (!valid){
+     $("#result").html("Not valid input");
+  }
+      $("#input").val('');
 });
